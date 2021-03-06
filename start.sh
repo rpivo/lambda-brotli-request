d1405d7f067b18fa11502c0d2443352c84cd2f53
Ryan <ryanpivovar@gmail.com>
2021-03-05 22:01:45 -0500

    format start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
-source ./sh/env.sh;
-rm -rf package && mkdir package;
-rm lambda.zip;
-docker start bundler || docker run -dt --name bundler -v $LOCAL_PATH:/bind-mount amazonlinux;
-docker exec -i bundler bash < sh/build.sh;
-docker stop bundler;
+source ./sh/env.sh
+rm -rf package && mkdir package
+rm lambda.zip
+docker start brotli || docker run -dt --name brotli -v $LOCAL_PATH:/bind-mount amazonlinux
+docker exec -i brotli bash <sh/build.sh
+docker stop brotli
