12fbb144ce5e3552e61a0146b2b4d6d063c865e0
Ryan <ryanpivovar@gmail.com>
2021-03-05 22:02:02 -0500

    add conditional zipping to build.sh

--- sh/build.sh
+++ sh/build.sh
@@ -1,12 +1,16 @@
 # prepare environment
-yum update -y;
-yum install -y zip;
-amazon-linux-extras install -y python3.8;
-python3.8 -m pip install -U pip;
+yum update -y
+yum install -y zip
+amazon-linux-extras install -y python3.8
+python3.8 -m pip install -U pip
 
 # install python dependencies
 # Ex: pip3 install pytrends --target ./bind-mount/package --upgrade;
 
 # zip
-cd bind-mount/package && zip -r ../lambda.zip . && cd ..;
-zip -g lambda.zip lambda_function.py;
+if [ -z "$(ls -A bind-mount/package)" ]; then
+  cd bind-mount && zip lambda.zip lambda_function.py
+else
+  cd bind-mount/package && zip -r ../lambda.zip . && cd ..
+  zip -g lambda.zip lambda_function.py
+fi
